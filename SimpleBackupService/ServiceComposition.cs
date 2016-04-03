c3cd819cfaa79aa8a03c062b260c50706d706540
pschumann.mail@googlemail.com <pschumann.mail@googlemail.com>
2016-04-03 12:40:30 +0200

    Added Logging after compostion

--- SimpleBackupService/ServiceComposition.cs
+++ SimpleBackupService/ServiceComposition.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SimpleBackupService.Contracts;
+using System.Diagnostics;
 
 namespace SimpleBackupService
 {
@@ -19,6 +20,15 @@ namespace SimpleBackupService
                 service.Config = new Configuration(container.GetExportedValueOrDefault<IBackupConfig>());
                 service.WebApi = new WebApi(container.GetExportedValueOrDefault<IBackupWebApi>());
                 service.Log = new Logging(container.GetExportedValueOrDefault<IBackupLogging>());
+
+                var message = new StringBuilder();
+                message.AppendLine("Composition finished!");
+                message.AppendLine("Status:");
+                message.AppendFormat("\tConfig: {0}\n", service.Config != null ? "found" : "not found");
+                message.AppendFormat("\tWebApi: {0}\n", service.WebApi != null ? "found" : "not found");
+                message.AppendFormat("\tLog: {0}\n", service.Log != null ? "found" : "not found");
+
+                EventLog.WriteEntry("SimpleBackupService", message.ToString());
             }
             catch { }
         }
